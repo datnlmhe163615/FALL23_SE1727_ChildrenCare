b7cff4a3801205498225a048fdebb5ba7c11335b
datnlmhe163615 <datnlmhe163615@fpt.edu.vn>
2023-09-25 16:23:06 +0700

    login update DAo

--- ChildCare/src/java/DAO/AccountDAO.java
+++ ChildCare/src/java/DAO/AccountDAO.java
@@ -13,7 +13,7 @@ import java.sql.ResultSet;
  *
  * @author mihxdat
  */
-public class AccountDAO extends DBContext{
+public class AccountDAO extends DBContext {
 
     public Account login(String email, String password) {
         String query = "select * from Account\n"
@@ -24,6 +24,22 @@ public class AccountDAO extends DBContext{
             ps.setString(1, email);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Account account = Account.builder()
+                        .id(rs.getInt(1))
+                        .email(rs.getString(2))
+                        .password(rs.getString(3))
+                        .fullname(rs.getString(4))
+                        .avatar(rs.getString(5))
+                        .mobile(rs.getString(6))
+                        .gender(rs.getBoolean(7))
+                        .address(rs.getString(8))
+                        .status(rs.getInt(9))
+                        .created_at(rs.getDate(10))
+                        .updated_at(rs.getDate(11))
+                        .build();
+                return account;
+            }
         } catch (Exception e) {
         }
         return null;
