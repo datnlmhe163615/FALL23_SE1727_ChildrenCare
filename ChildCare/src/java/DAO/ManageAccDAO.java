1d8cbe16d9443f80d61adcd14b13f7767f3bed27
datnlmhe163615 <datnlmhe163615@fpt.edu.vn>
2023-10-11 23:47:33 +0700

    update

--- ChildCare/src/java/DAO/ManageAccDAO.java
+++ ChildCare/src/java/DAO/ManageAccDAO.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
+
 import Model.Account;
 import java.sql.*;
 import java.util.*;
@@ -12,9 +13,9 @@ import java.util.*;
  * @author mihxdat
  */
 public class ManageAccDAO {
-    
+
     Connection conn = DBContext.Dat_DBContext.CreateConnection();
-    
+
     public Account getAccountByLogin(String email, String pass) {
         Account acc = new Account();
         String sql = "select * from account where email = ? and [password] = ?";
@@ -26,6 +27,7 @@ public class ManageAccDAO {
             if (rs.next()) {
                 acc.setId(rs.getInt("id"));
                 acc.setEmail(rs.getString("email"));
+                acc.setPassword(rs.getString("password"));
                 acc.setRole(rs.getString("role"));
                 acc.setFullname(rs.getString("fullname"));
                 acc.setAvatar(rs.getString("avatar"));
@@ -44,7 +46,51 @@ public class ManageAccDAO {
 
         return null;
     }
+
+    public void RegisterNewAccount(String email, String password, String fullname) {
+        try {
+            String sql = "INSERT INTO Account(email, role, password,fullname,gender,status)\n"
+                    + "VALUES (?, ?, ?,?, ?, ?);";
+            PreparedStatement stm = conn.prepareStatement(sql);
+            stm.setString(1, email);
+            stm.setString(2, "3");
+            stm.setString(3, password);
+            stm.setString(4, fullname);
+            stm.setString(5, "0");
+            stm.setString(6, "1");
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     
+    public ArrayList<Account> list() {
+        ArrayList<Account> accounts = new ArrayList<>();
+        try {
+            String sql = "select acc_id, fullname, email, gender, phone, address, role_id from account";
+            PreparedStatement stm = conn.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Account acc = new Account();
+                acc.setId(rs.getInt("id"));
+                acc.setEmail(rs.getString("email"));
+                acc.setRole(rs.getString("role"));
+                acc.setFullname(rs.getString("fullname"));
+                acc.setAvatar(rs.getString("avatar"));
+                acc.setMobile(rs.getString("mobile"));
+                acc.setGender(rs.getBoolean("gender"));
+                acc.setAddress(rs.getString("address"));
+                acc.setStatus(rs.getInt("status"));
+                acc.setCreated_at(rs.getDate("createdat"));
+                acc.setUpdated_at(rs.getDate("updatedat"));
+                accounts.add(acc);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return accounts;
+    }
+
     public static void main(String[] args) {
         String email = "customer1@gmail.com"; // Thay bằng email thực tế
         String pass = "123";
@@ -57,5 +103,5 @@ public class ManageAccDAO {
             System.out.println("Không tìm thấy tài khoản.");
         }
     }
-            
+
 }
