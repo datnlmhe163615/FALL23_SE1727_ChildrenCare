932b31d02ac37f1112a8b773e9880b178560ba81
dungnthe163811 <dungnthe163811@fpt.edu.vn>
2023-10-15 19:44:35 +0700

    services

--- ChildCare/src/java/Controller/services.java
+++ ChildCare/src/java/Controller/services.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
  *
  * @author asus
  */
-public class sevices extends HttpServlet {
+public class services extends HttpServlet {
    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
