c23a5d63c81d734f6b199f4a5d76e6ca118b3cf9
dungnthe163811 <dungnthe163811@fpt.edu.vn>
2023-10-29 14:03:47 +0700

    Update ReservationList.java

--- ChildCare/src/java/Controller/ReservationList.java
+++ ChildCare/src/java/Controller/ReservationList.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
  * @author iNFJZ
  */
 public class ReservationList extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
