264d43d193b59ed73b0ab51c34d9499c82da41ee
dungnthe163811 <dungnthe163811@fpt.edu.vn>
2023-11-08 23:59:43 +0700

    new

--- ChildCare/src/java/DAO/ReservationDAO.java
+++ ChildCare/src/java/DAO/ReservationDAO.java
@@ -5,9 +5,9 @@
 package DAO;
 
 import Model.Account;
-import Model.Reservation;
-import Model.ReservationItem;
-import Model.Service;
+import Model.ReservationItem_1;
+import Model.Reservation_1;
+import Model.Service_2;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -19,8 +19,8 @@ import java.util.ArrayList;
  */
 public class ReservationDAO extends DBContext.DBContext {
 
-    public ArrayList<Reservation> getReservation(int index) {
-        ArrayList<Reservation> list = new ArrayList<>();
+    public ArrayList<Reservation_1> getReservation(int index) {
+        ArrayList<Reservation_1> list = new ArrayList<>();
         String sql = "SELECT * FROM [Reservation] r inner join Account a on r.customerId = a.id\n"
                 + "ORDER BY r.id OFFSET ? ROWS FETCH NEXT 9 ROWS ONLY";
         try {
@@ -28,7 +28,7 @@ public class ReservationDAO extends DBContext.DBContext {
             ps.setInt(1, (index - 1) * 6);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                list.add(new Reservation(rs.getInt(1), rs.getInt(3), rs.getString(4), rs.getString(5), rs.getBoolean(6), rs.getString(7),
+                list.add(new Reservation_1(rs.getInt(1), rs.getInt(3), rs.getString(4), rs.getString(5), rs.getBoolean(6), rs.getString(7),
                         rs.getInt(8), rs.getFloat(9), rs.getDate(10), rs.getDate(11), new Account(rs.getString(16), rs.getString(17))));
             }
         } catch (SQLException e) {
@@ -50,8 +50,8 @@ public class ReservationDAO extends DBContext.DBContext {
         return 0;
     }
 
-    public ArrayList<Reservation> getReservationDetail(int id) {
-        ArrayList<Reservation> list = new ArrayList<>();
+    public ArrayList<Reservation_1> getReservationDetail(int id) {
+        ArrayList<Reservation_1> list = new ArrayList<>();
         String sql = "SELECT * FROM [Reservation] r inner join ReservationItem d on r.id = d.reservationId \n"
                 + "inner join Service s on s.id = d.serviceId\n"
                 + "where r.id = ?";
@@ -60,9 +60,9 @@ public class ReservationDAO extends DBContext.DBContext {
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                list.add(new Reservation(rs.getInt(1),
-                        new ReservationItem(rs.getInt(12), rs.getInt(13), rs.getInt(14), rs.getInt(15), rs.getFloat(16), rs.getInt(17), rs.getDate(18), rs.getDate(19)),
-                        new Service(rs.getInt(21), rs.getString(23), rs.getString(24))));
+                list.add(new Reservation_1(rs.getInt(1),
+                        new ReservationItem_1(rs.getInt(12), rs.getInt(13), rs.getInt(14), rs.getInt(15), rs.getFloat(16), rs.getInt(17), rs.getDate(18), rs.getDate(19)),
+                        new Service_2(rs.getInt(21), rs.getString(23), rs.getString(24))));
             }
 
         } catch (SQLException e) {
