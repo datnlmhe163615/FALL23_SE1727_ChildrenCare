c7b652ec5dcaef78c6bb969186a4cf208c51eb99
dungnthe163811 <dungnthe163811@fpt.edu.vn>
2023-11-08 12:21:10 +0700

    new

---
+++ ChildCare/src/java/Controller/DoctorList.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller;
+
+import DAO.AccountDAO;
+import Model.Account;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author asus
+ */
+public class DoctorList extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       AccountDAO dao = new AccountDAO(); 
+       ArrayList<Account> acc = dao.getDoctor(); 
+       request.setAttribute("acc", acc);
+       request.getRequestDispatcher("doctor.jsp").forward(request, response); 
+
+    } 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
